561aa5d1f252f5ee9d9d5660711d94826e41c6a2
rmangisi <rmangisi@f3d7b4de-d455-6c8d-2623-c28d14758f8d>
2010-09-15 15:39:59 +0000

    added new classes LoginPageActivity.class,Login.xml,changed icons and put nicer icons for home screen.For now to access the program you can just click login without puting  a username.

--- trunk/src/com/labproject/touchmenuversion1/HomePageActivity.java
+++ trunk/src/com/labproject/touchmenuversion1/HomePageActivity.java
@@ -2,36 +2,52 @@ package com.labproject.touchmenuversion1;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class HomePageActivity extends MenuActivity {
-    /** Called when the activity is first created. */
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
-        Button NavigateToCategory = (Button) findViewById(R.id.OrderButton);
-        NavigateToCategory.setOnClickListener(  new View.OnClickListener() 
-        
-        {
-        public void onClick(View v) {
-        // navigate to category page//consider using the clickhandler with switch
-        	setContentView(R.layout.category);
-        }
-        }
-        
-        );
-           
-        
-        
-        
-        
-        
-    }
-    
-    
-        
-    
+//public class HomePageActivity  extends MenuActivity implements OnClickListener{
+
+ 
+ /*
+protected void onCreate(Bundle savedValues)
+{
+ //   super.onCreate(savedInstanceState);
+ //   setContentView(R.layout.category);
+   //ImageButton NavigateToCategory = (ImageButton) findViewById(R.id.ordericon);
+    //ImageButton NavigateToCategory = (ImageButton) findViewById(R.id.ordericon);
+   // NavigateToCategory.setOnClickListener(  new View.OnClickListener() 
+    ImageButton button =(ImageButton)findViewById(R.id.ordericon);
+    button.setOnClickListener( this);
 }
+    public void onClick(View b) {
+    // navigate to category page//consider using the clickhandler with switch
+    	setContentView(R.layout.login);
+    };
+    
+    
+} 
+*/
+	
+	public class HomePageActivity extends MenuActivity {
+	    /** Called when the activity is first created. */
+		
+	    @Override
+	    public void onCreate(Bundle rod)
+	    {
+	        super.onCreate(rod);
+	     setContentView(R.layout.category);
+	       //ImageButton NavigateToCategory = (ImageButton) findViewById(R.id.ordericon);
+	        ImageButton NavigateToCategory = (ImageButton) findViewById(R.id.ordericon);
+	        NavigateToCategory.setOnClickListener(  new View.OnClickListener() 
+	        
+	        {
+	        public void onClick(View v) {
+	        // navigate to category page//consider using the clickhandler with switch
+	        	setContentView(R.layout.category);
+	        }
+	        }
+	        
+	        );
+	    }
+	}
